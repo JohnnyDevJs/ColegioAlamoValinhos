b217caf1616d79d7804f52984919543da0f80b45
JohnnyDevJs <johnnydev.js@gmail.com>
2021-04-16 12:56:31 -0300

    mudanças

--- src/components/Header/Mobile/Toggle/index.tsx
+++ src/components/Header/Mobile/Toggle/index.tsx
@@ -6,7 +6,7 @@ import { MobileContent, Logo } from './styles'
 import Nav from '@/components/Header/Mobile/Nav'
 import Info from '@/components/Header/Mobile/Info'
 
-import AlamoValinhosLogo from '../../../../assets/alamo-logo-valinhos.svg'
+import AlamoValinhosLogo from '../../../../../public/assets/alamo-logo-valinhos.svg'
 
 const Toggle: React.FC = () => {
   const handleCloseToggle = useCallback(() => {
