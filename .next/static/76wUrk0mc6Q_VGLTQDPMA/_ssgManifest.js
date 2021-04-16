942ccb76fb8b9d5de025608750a5b8e707e76291
JohnnyDevJs <johnnydev.js@gmail.com>
2021-04-16 16:04:05 -0300

    production

--- .next/static/76wUrk0mc6Q_VGLTQDPMA/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fo-colegio\u002Fgaleria","\u002Fdiferenciais\u002F[slug]","\u002Fo-colegio\u002Fblog\u002F[slug]","\u002Fcursos\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
